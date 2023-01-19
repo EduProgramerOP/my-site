164593a3f990341fd907ee37cc193b8fe1a1b58c
EduProgramerOP <102773782+EduProgramerOP@users.noreply.github.com>
2023-01-19 14:25:48 -0600

    Update Fuctions.js

--- JS/Fuctions.js
+++ JS/Fuctions.js
@@ -1,3 +1,17 @@
+// Increments the delay on each item.
+const programas=``
+$('.rolldown-list li').each(function () {
+  var delay = ($(this).index() / 4) + 's';
+  $(this).css({
+    webkitAnimationDelay: delay,
+    mozAnimationDelay: delay,
+    animationDelay: delay
+  });
+});
 
-todo=`todo jala`;
-document.write(todo) ;
+$('#btnReload').click(function () {
+  $('#myList').removeClass('rolldown-list');
+  setTimeout(function () {
+    $('#myList').addClass('rolldown-list');
+  }, 2);
+});
